c3a4b0cf4b3faaf6c6aba9e2c9286436fc23e3eb
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2026-02-09 15:49:58 +0100

    open_earable_python/__init__.py: cleaned up init

--- src/open_earable_python/__init__.py
+++ src/open_earable_python/__init__.py
@@ -1,17 +1,11 @@
 from .dataset import (
-    SensorAccessor,
     SensorDataset,
     load_recordings,
     display_recordings,
-    LABELS,
-    COLORS,
 )
 
 __all__ = [
-    "SensorAccessor",
     "SensorDataset",
     "load_recordings",
     "display_recordings",
-    "LABELS",
-    "COLORS",
 ]
