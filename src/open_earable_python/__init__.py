06105ae6daad2a200157dba9df82a4df269c6f15
Dennis Moschina <45356478+DennisMoschina@users.noreply.github.com>
2026-02-09 16:02:42 +0100

    src/open_earable_python/dataset.py: removed plot and display funciton

--- src/open_earable_python/__init__.py
+++ src/open_earable_python/__init__.py
@@ -1,11 +1,9 @@
 from .dataset import (
     SensorDataset,
     load_recordings,
-    display_recordings,
 )
 
 __all__ = [
     "SensorDataset",
     "load_recordings",
-    "display_recordings",
 ]
